6bb771d897331d8d9be9ab22ef25d06a87a6c09e
Alexander Martinz <amartinz@shiftphones.com>
2024-08-14 16:30:44 +0200

    (SHIFT) tools: releasetools: sign_target_files_apks: also rewrite ro.build.keys
    Change-Id: Idb81138293f5e265cabfbfe4f36ee7cd4e782f26
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -1140,7 +1140,7 @@ def RewriteProps(data):
         pieces = value.split("/")
         pieces[-1] = EditTags(pieces[-1])
         value = "/".join(pieces)
-      elif key == "ro.build.description":
+      elif key == "ro.build.description" or key == "ro.build.keys":
         pieces = value.split()
         assert pieces[-1].endswith("-keys")
         pieces[-1] = EditTags(pieces[-1])
